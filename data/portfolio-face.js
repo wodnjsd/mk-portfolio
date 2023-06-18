a1959d32be2455724734ac00cb5193b1365168bc
Jaewon Han <wodnjsd@gmail.com>
2023-06-18 10:20:54 +0100

    face description

--- data/portfolio-face.js
+++ data/portfolio-face.js
@@ -7,7 +7,7 @@ import image6 from '@/public/assets/portfolios/full-face/3_after.jpeg'
 
 
 export default [
-  { id: 1, before: image1, after: image2, description: "Beautiful new smile with crowns, whitening etc." },
-  { id: 2, before: image3, after: image4, description: "Lovely smile" },
-  { id: 3, before: image5, after: image6, description: "Great smile" },
+  { id: 1, before: image1, after: image2, description: "5 porcelain crowns after root canal treatments and one re-root canal" },
+  { id: 2, before: image3, after: image4, description: "Stabilised with extraction and fillings, then a partial cobalt-chrome denture" },
+  { id: 3, before: image5, after: image6, description: "New smile with new complete dentures" },
 ];
