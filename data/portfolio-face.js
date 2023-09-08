8a786089ca272109932743b5d2c338651be6d29a
Jaewon Han <wodnjsd@gmail.com>
2023-09-08 23:34:53 +0100

    full-face case added

--- data/portfolio-face.js
+++ data/portfolio-face.js
@@ -4,10 +4,13 @@ import image3 from '@/public/assets/portfolios/full-face/2_before.jpeg'
 import image4 from '@/public/assets/portfolios/full-face/2_after.jpeg'
 import image5 from '@/public/assets/portfolios/full-face/3_before.jpeg'
 import image6 from '@/public/assets/portfolios/full-face/3_after.jpeg'
+import image7 from '@/public/assets/portfolios/full-face/4_before.jpeg'
+import image8 from '@/public/assets/portfolios/full-face/4_after.jpeg'
 
 
 export default [
   { id: 1, before: image1, after: image2, description: "5 porcelain crowns after root canal treatments and one re-root canal" },
   { id: 2, before: image3, after: image4, description: "Stabilised with extraction and fillings, then a partial cobalt-chrome denture" },
   { id: 3, before: image5, after: image6, description: "New smile with new complete dentures" },
+  { id: 4, before: image7, after: image8, description: "New confident smile with an adhesive bridge to replace missing front tooth" },
 ];
--- data/portfolio-tx.js
+++ data/portfolio-tx.js
@@ -1,10 +1,12 @@
 import image7 from "@/public/assets/portfolios/7.png";
 import image8 from "@/public/assets/portfolios/8.jpeg";
 import image9 from "@/public/assets/portfolios/9.jpeg";
+import image11 from "@/public/assets/portfolios/11.png"
 
 
 export default [
   { id: 1, image: image8, description: "Posterior white filling" },
   { id: 2, image: image7, description: "Anterior white filling" },
   { id: 3, image: image9, description: "Posterior white filling" },
+  { id: 2, image: image11, description: "Anterior bridge" },
 ];
